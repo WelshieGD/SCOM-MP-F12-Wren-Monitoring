3b0632d4a16ba915d1ff3ac9185c8bda9c224971
Graham Davies <graham@grahamdavies.info>
2023-05-08 13:58:29 +0100

    script

---
+++ F12.Wren.Monitoring/Health Model/Scripts/NumFilesOldestFile/AllQueuesStat.ps1
@@ -0,0 +1,46 @@
+﻿param 
+(
+	[string]$queuesPath
+)
+
+
+
+# SCOM Specific Header
+$api = New-Object -comObject 'MOM.ScriptAPI'
+$api.LogScriptEvent('AllQueuesStats.ps1', 101, 4, $folder)
+
+
+
+$files = Get-ChildItem -Path $queuesPath -Recurse | where {$_.psIsContainer -eq $false}
+
+if ($files -ne $null) {$count = $files.count}
+else {$count = 0}
+
+$size = 0
+$oldestfile = 0
+
+foreach ($file in $files)
+{
+	$size += $file.Length
+
+	$ageinMinutes = ((Get-Date) - ($file.creationTime)).TotalMinutes
+
+	if ($ageinMinutes -gt $oldestfile) {$oldestfile = $ageinMinutes}
+
+}
+
+$bag = $api.CreatePropertyBag()
+$bag.AddValue('FileCount', $count)
+$bag.AddValue('OldestFile', $oldestfile)
+$bag.AddValue('TotalSize', $size)
+
+
+# $bag
+
+# For Testing
+$api.return($bag)
+
+
+
+
+
